c7d0ddb099c290f6e2d294a1af8692f48f4f1030
isaiah dahl <isaiahdahl@me.com>
2022-03-15 15:46:27 -0700

    ref #116 use rollup for an esbuild

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+// eslint-disable-next-line import/no-unresolved
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import pegjs from 'rollup-plugin-pegjs';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    pegjs(),
+    external(),
+    resolve({
+      preferBuiltins: true,
+    }),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      exclude: '**/__tests__/**',
+      clean: true,
+    }),
+    commonjs({
+      include: ['node_modules/**'],
+    }),
+
+  ],
+};
