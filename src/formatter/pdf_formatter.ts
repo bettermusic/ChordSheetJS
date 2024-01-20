cc86566bc3b91c0dffabf050c2b8b5e9c29bd6d8
isaiahdahl <isaiahdahl@me.com>
2024-01-20 00:14:01 -0800

    feat: get a working version of rendering a pdf via text width calculating

--- src/formatter/pdf_formatter.ts
+++ src/formatter/pdf_formatter.ts
@@ -1,235 +1,257 @@
 import Formatter from './formatter';
 import jsPDF from 'jspdf';
-import { evaluate, hasChordContents, hasTextContents, isChordLyricsPair, isComment, isEvaluatable, isTag, lineHasContents, renderChord } from '../template_helpers';
-import ChordLyricsPair from '../chord_sheet/chord_lyrics_pair';
-import Tag from '../chord_sheet/tag';
-import Evaluatable from '../chord_sheet/chord_pro/evaluatable';
-import Item from '../chord_sheet/item';
+import { isChordLyricsPair, isComment, lineHasContents } from '../template_helpers';
+import Song from '../chord_sheet/song';
 
-/**
- * Formats a song into a ChordPro chord sheet
- */
 class PdfFormatter extends Formatter {
-  song: any;
+  song: Song = new Song();
   y: number = 0;
+  x: number = 0;
   doc: jsPDF = new jsPDF;
+  startTime: number = 0;
+  currentColumn: number = 1;
+  columnWidth: number = 0;
+
+  // Configuration settings for the PDF document
   get pdfConfiguration() {
     return {
+      // Font settings for various elements
       fonts: {
-        title: { // Default is “Times-Bold” at size 14.
-          name: 'Helvetica',
-          size: 18,
-          color: 'black',
-        },
-
-        subtitle: { // Default is the setting for text.
-          name: 'Helvetica',
-          size: 14,
-          color: 'black',
-        },
-
-        metadata: { // (made up) Default is the setting for text.
-          name: 'Helvetica',
-          size: 10,
-          color: 'black',
-        },
-
-        text: { // Default is “Times-Roman” at size 12.
-          name: 'Helvetica',
-          size: 10,
-          color: 'black',
-        },
-
-        chord: { // Default is “Helvetica-Oblique” at size 10.
-          name: 'Helvetica',
-          size: 10,
-          color: 'red',
-        },
-
-        comment: { // Default is “Helvetica” at size 12, with a grey background.
-          name: 'Helvetica',
-          size: 10,
-          color: 'black',
-        },
-
-        annotation: { // Defaults to the chord font.
-          name: 'Helvetica',
-          size: 10,
-          color: 'black',
-        }
+        title: { name: 'helvetica', style: 'bold', size: 24, color: 'black' },
+        subtitle: { name: 'helvetica', style: 'normal', size: 10, color: 100 },
+        metadata: { name: 'helvetica', style: 'normal', size: 10, color: 100 },
+        text: { name: 'helvetica', style: 'normal', size: 10, color: 'black' },
+        chord: { name: 'helvetica', style: 'bold', size: 10, color: 'black' },
+        comment: { name: 'helvetica', style: 'bold', size: 10, color: 'black' },
+        annotation: { name: 'helvetica', style: 'normal', size: 10, color: 'black' }
       },
-
-      margintop: 80,
+      // Layout settings
+      margintop: 40,
       marginbottom: 40,
       marginleft: 40,
       marginright: 40,
+      lineHeight: 5,
+      chordLyricSpacing: 0,
+      linePadding: 10,
+      numberOfSpacesToAdd: 2,
+      columnCount: 2,
+      columnWidth: 0,
+      columnSpacing: 25
     };
   }
-  
-  format(song) {
+
+  // Main function to format and save the song as a PDF
+  format(song: Song) {
+    this.startTime = performance.now();
     this.song = song;
     this.y = this.pdfConfiguration.margintop;
     this.doc = this.setupDoc();
     this.formatTitles();
-    this.formatMetadata();
     this.formatParagraphs();
+    this.recordFormattingTime();
   }
-  
+
+  // Save the formatted document as a PDF file
   save() {
-    this.doc.save(`${this.song.title || 'untitled'}.pdf`)
+    this.doc.save(`${this.song.title || 'untitled'}.pdf`);
   }
 
+  // Generate the PDF as a Blob object
   async generatePDF() {
-    return new Promise((resolve, reject) => {
-        const blob = this.doc.output('blob');
-        resolve(blob);
+    return new Promise((resolve) => {
+      const blob = this.doc.output('blob');
+      resolve(blob);
     });
-}
+  }
 
+  // Document setup configurations
   setupDoc() {
     const doc = new jsPDF('portrait', 'px');
     doc.setLineWidth(0);
-    doc.setDrawColor(0,0,0,0)
+    doc.setDrawColor(0,0,0,0);
+    this.columnWidth = (doc.internal.pageSize.getWidth() - this.pdfConfiguration.marginleft - this.pdfConfiguration.marginright - ((this.pdfConfiguration.columnCount - 1) * this.pdfConfiguration.columnSpacing)) / this.pdfConfiguration.columnCount;
+    this.x = this.pdfConfiguration.marginleft;
     return doc;
   }
+
+  // Formatting helpers
+  formatTitles() {
+    const { title, subtitle, artist, key } = this.song;
   
-  text(text, style) {
-    const { name: fontName, size, color } = this.pdfConfiguration.fonts[style];
-    const { marginleft } = this.pdfConfiguration;
-    this.doc.setFontSize(size);
-    this.doc.setTextColor(color);
-    this.doc.setFont(fontName);
-    this.doc.text(text, marginleft, this.y);
-    this.y += size;
+    this.y = this.pdfConfiguration.margintop;
+  
+    if (title) {
+      this.setFontStyle('title');
+      const titleDimensions = this.getTextDimensions(title);
+      this.doc.text(title, this.pdfConfiguration.marginleft, this.y);
+      this.y += titleDimensions.h - 5;
+    }
+  
+    if (artist || subtitle) {
+      this.setFontStyle('subtitle');
+      this.doc.text(artist + ' ' + subtitle, this.pdfConfiguration.marginleft, this.y);
+      let artistDimensions = this.getTextDimensions(artist + ' ' + subtitle);
+      this.y += artistDimensions.h;
+    }
+  
+    let metaDataString = `Key of ${key}`;
+    this.setFontStyle('metadata');
+    this.doc.text(metaDataString, this.pdfConfiguration.marginleft, this.y);
+    let metaDataDimensions = this.getTextDimensions(metaDataString);
+    this.y += metaDataDimensions.h + this.pdfConfiguration.linePadding;
+  }
+
+  formatParagraphs() {
+    const columnHeight = this.doc.internal.pageSize.getHeight() - this.pdfConfiguration.margintop - this.pdfConfiguration.marginbottom;
+    const bodyParagraphs = this.song.bodyParagraphs;
+  
+    bodyParagraphs.forEach((paragraph) => {
+      this.formatParagraph(paragraph, columnHeight);
+    });
   }
   
+  formatParagraph(paragraph, columnHeight) {
+    paragraph.lines.forEach((line) => {
+      if (lineHasContents(line)) {
+        if (this.y + this.pdfConfiguration.lineHeight > columnHeight) {
+          this.moveToNextColumn(columnHeight);
+        }
+        this.formatLine(line);
+        this.y += this.pdfConfiguration.lineHeight;
+      }
+    });
+  }
+
+  formatLine(line) {
+    let x = this.x;
+    let maxChordHeight = this.getMaxChordHeight(line);
+    const spaceWidth = this.getSpaceWidth();
+  
+    line.items.forEach((item) => {
+      if (isChordLyricsPair(item)) {
+        let chordWidth = 0;
+        let lyricWidth = 0;
+        let extraSpace = 0;
+  
+        // Render and position chords
+        if (item.chords) {
+          this.setFontStyle('chord');
+          let chordDimensions = this.getTextDimensions(item.chords);
+          chordWidth = chordDimensions.w;
+          let chordBaseline = this.y + maxChordHeight - chordDimensions.h;
+          this.doc.text(item.chords, x, chordBaseline);
+        }
+  
+        // Render and position lyrics
+        if (item.lyrics && item.lyrics.trim() !== '') {
+          this.setFontStyle('text');
+          let lyricDimensions = this.getTextDimensions(item.lyrics);
+          lyricWidth = lyricDimensions.w;
+          let lyricsY = this.y + maxChordHeight + this.pdfConfiguration.chordLyricSpacing;
+          this.doc.text(item.lyrics, x, lyricsY);
+        }
+  
+        // Calculate additional space for chord-lyric alignment
+        if (chordWidth > lyricWidth) {
+          let numberOfSpacesToAdd = this.pdfConfiguration.numberOfSpacesToAdd || 0;
+          extraSpace = numberOfSpacesToAdd * spaceWidth;
+        }
+  
+        // Update x for the next chord-lyric pair
+        x += Math.max(chordWidth, lyricWidth) + extraSpace;
+      }
+  
+      if (isComment(item)) {
+        this.formatComment(item.value);
+      }
+  
+      // Handle other item types...
+    });
+  
+    // Update y for the next line
+    this.y += maxChordHeight + this.pdfConfiguration.chordLyricSpacing + this.pdfConfiguration.linePadding;
+  }  
+
+  formatComment(commentText) {
+    this.setFontStyle('comment');
+    const textY = this.y;
+  
+    // Print comment text
+    this.doc.text(commentText, this.x, textY);
+  
+    // Underline the comment
+    const textWidth = this.getTextDimensions(commentText).w;
+    this.doc.setDrawColor(0);
+    this.doc.setLineWidth(0.5);
+    this.doc.line(this.x, textY + 1, this.x + textWidth, textY + 1);
+  
+    // Update y for next element
+    this.y += this.getTextDimensions(commentText).h;
+  }  
+
+  // Utility functions
   spacer(size) {
     this.y += size;
   }
   
   getFontConfiguration(objectType) {
-    const { name: fontName, size: fontSize, color } = this.pdfConfiguration.fonts[objectType];
-    return { color, fontName, fontSize };
+    return this.pdfConfiguration.fonts[objectType];
   }
   
-  formatTitles() {
-    const { title, subtitle } = this.song;
-    
-    if (title) this.text(title, 'title');
-    if (subtitle) this.text(subtitle, 'subtitle');
-    if (title || subtitle) this.spacer(20);
-  }
-  
-  formatMetadata() {
-    const { metadata } = this.song.metadata;
-
-    const keys =
-      Object
-        .keys(metadata)
-        .filter((key) => !['title', 'subtitle'].includes(key));
-
-    if (keys.length > 0) {
-      keys.forEach((key) => {
-        this.text(`${key}: ${metadata[key]}`, 'metadata');
-      });
-
-      this.spacer(50);
-    }
-  }
-  
-  formatParagraphs() {
-    const bodyParagraphs =
-      this.configuration.expandChorusDirective ?
-        this.song.expandedBodyParagraphs :
-        this.song.bodyParagraphs;
-
-    bodyParagraphs.forEach((paragraph, index, { length }) => {
-      this.formatParagraph(paragraph);
-    });
-  }
-  
-  formatParagraph(paragraph) {
-    paragraph.lines.forEach((line) => {
-      if (lineHasContents(line)) {
-        this.formatLine(line);
+  getMaxChordHeight(line) {
+    let maxHeight = 0;
+    line.items.forEach((item) => {
+      if (isChordLyricsPair(item) && item.chords) {
+        this.setFontStyle('chord');
+        let dimensions = this.getTextDimensions(item.chords);
+        maxHeight = Math.max(maxHeight, dimensions.h);
       }
     });
+    return maxHeight;
   }
   
-  formatLine(line: any) {
-    const chordFont = this.getFontConfiguration('chord');
-    const lyricFont = this.getFontConfiguration('text');
-    const commentFont = this.getFontConfiguration('comment');
-    const annotationFont = this.getFontConfiguration('annotation');
-    const { key, useUnicodeModifiers, normalizeChords } = this.configuration;
-    const { metadata } = this.song;
-    
-    const topLine: string[] = [];
-    const bottomLine: string[] = [];
-    
-    if (hasChordContents(line)) {
-      line.items.forEach((item: ChordLyricsPair) => {
-        if (isChordLyricsPair(item)) {
-          const chord: string = renderChord(
-            item.chords,
-            line,
-            this.song,
-            {
-              renderKey: key,
-              useUnicodeModifier: useUnicodeModifiers,
-              normalizeChords,
-            },
-          );
-          topLine.push(chord);
-        }
-      });
-    }
-    
-    if (hasTextContents(line)) {
-      line.items.forEach((item: Item) => {
-        if (isChordLyricsPair(item)) {
-          const chordLyricsPairItem = item as ChordLyricsPair;
-          bottomLine.push(chordLyricsPairItem.lyrics ?? '');
-        } else if (isTag(item)) {
-          const tagItem = item as Tag;
-          if (isComment(tagItem)) {
-            bottomLine.push(tagItem.value ?? '');
-          } else if (tagItem.hasRenderableLabel()) {
-            bottomLine.push(tagItem.value);
-          }
-        } else if (isEvaluatable(item)) {
-          const evaluatableItem = item as Evaluatable;
-          const evaluated: string = evaluate(evaluatableItem, metadata, this.configuration);
-          bottomLine.push(evaluated);
-        }
-      });
-    }
-    
-    this.table([topLine, bottomLine].filter(line => line.length > 0));
+  getTextDimensions(text) {
+    return this.doc.getTextDimensions(text);
   }
   
-  table(rows) {
-    const { size: fontSize } = this.pdfConfiguration.fonts.text;
-    
-    const styles = {
-      autoSize: true,
-      printHeaders: false,
-      columnWidths: 80,
-      fontSize,
-    };
-    
-    const data = rows.map((row) => (
-      row.reduce((acc, cell, index) => ({
-        ...acc,
-        [`r${index}`]: cell,
-      }), {})
-    ));
-    
-    const headers = rows[0].reduce((acc, cell, index) => [...acc, `r${index}`], []);
-    const { marginleft } = this.pdfConfiguration;
-    this.doc.table(marginleft, this.y, data, headers, styles);
-    this.y += (rows.length * fontSize * 1.5);
+  setFontStyle(style) {
+    const { name: fontName, style: fontStyle, size, color } = this.pdfConfiguration.fonts[style];
+    this.doc.setFontSize(size);
+    this.doc.setTextColor(color);
+    this.doc.setFont(fontName, fontStyle);
   }
+  
+  recordFormattingTime() {
+    const endTime = performance.now();
+    const timeTaken = ((endTime - this.startTime) / 1000).toFixed(5);
+    
+    this.setFontStyle('text');
+    this.doc.setTextColor(100);
+  
+    const pageWidth = this.doc.internal.pageSize.getWidth();
+    const timeTextWidth = this.getTextDimensions(timeTaken + 's').w;
+    const timeTextX = pageWidth - timeTextWidth - this.pdfConfiguration.marginright;
+    const timeTextY = this.pdfConfiguration.margintop / 2;
+  
+    this.doc.text(timeTaken + 's', timeTextX, timeTextY);
+  }
+  
+  moveToNextColumn(columnHeight) {
+    this.currentColumn++;
+    if (this.currentColumn > this.pdfConfiguration.columnCount) {
+      this.doc.addPage();
+      this.currentColumn = 1;
+      this.x = this.pdfConfiguration.marginleft;
+    } else {
+      this.x = (this.currentColumn - 1) * this.columnWidth + this.pdfConfiguration.columnSpacing + this.pdfConfiguration.marginleft;
+    }
+    this.y = this.pdfConfiguration.margintop;
+  }
+  
+  getSpaceWidth() {
+    return this.doc.getTextDimensions(' ').w;
+  }
+  
 }
 
 export default PdfFormatter;
--- test/formatter/pdf/pdf-dev.js
+++ test/formatter/pdf/pdf-dev.js
@@ -3,20 +3,124 @@ import { ChordProParser, PdfFormatter } from '../../../lib';
 const chordpro = `
 {title: Kingdom}
 {subtitle: (feat. Naomi Raine & Chandler Moore)}
-{composer: Maverick City}
-{lyricist: Kirk Franklin}
+{artist: Maverick City}
+{author: Kirk Franklin}
 {key: F}
 
 
-{comment: Intro (2x)}
-[Gm][F][/][/][|][C#dim7][Dm][/][/][|]
+{c:Intro (2x)}
+[Gm][F][/][/][|][Dbdim7][Dm][/][/][|]
 
-{comment: Verse 1 *1}
+{c:Verse 1 *1}
 My [Dm7]heart has always [C/E]longed for something[F] more
 I [Dm7]searched the stars to [Bb]knock on Heaven's [F2]door
 Cre - [Dm7]ation groans for [C2/E]God to be re [F2]- vealed
 [Dm7]Every wound we [Bb]carry will be[F2] healed
 My eyes on the [G7/B]Son, Lord Your will be [Bbm6]done
+
+{c:Chorus 1 *2}
+Thine is the [Gm]King - [F]dom, the power, the [Dbdim7]glo - [Dm]ry
+Forever and [Gm]e - [F]ver, He finished my [Dbdim7]sto - [Dm]ry
+We’re singing [Gm]free - [F/A]dom, our testi - [Dbdim7]mo - [Dm]ny
+We’ll be singing for - [G13]ev - [Gm7/C]er, a - [F]men[A7(#9)/E][Dm11]
+We’ll be [Dm/C]singing for - [G13]ever and [G13(#5)]ever, [Gm7/C] a - [F]men
+
+{c:Verse 2 *3}
+[Dm7]Beautiful each [C/E]color that He [F]made
+Your [Dm7]love's the only [Bb]remedy for[F2] hate
+[Dm7]You'll return to [C2/E]set the pris'ners [F2]free
+‘Til [Dm7]then Your will on [Bb]Earth be done in[F2] me
+My eyes on the [G7/B]Son, Lord Your will be [Bbm6]done
+
+{c:Chorus 2 *4a}
+Thine is the [Gm]King - [F]dom, the power the [Dbdim7]glo - [Dm]ry
+Forever and [Gm]e - [F]ver, He finished my [Dbdim7]sto - [Dm]ry
+We’re singing [Gm]free - [F/A]dom, our testi - [Dbdim7]mo - [Dm]ny
+We’ll be singing for - [G13]ev - [Gm7/C]er, a - [F]men[x]
+
+{c:Chorus 2 *4b}
+Thine is the [Gm]King - [F]dom, the power the [Dbdim7]glo - [Dm]ry
+Forever and [Gm]e - [F]ver, He finished my [Dbdim7]sto - [Dm]ry
+We’re singing [Gm]free - [F/A]dom, our testi - [Dbdim7]mo - [Dm]ny
+We’ll be singing for - [G13]ev - [Gm7/C]er, a - [F]men[A7(#9)][Dm11]
+We’ll be [Dm7/C]singing for - [G13/B]ever and [G7(#5)]ever, [Gm7/C] a - [F]men[A7(#9)][Dm11]
+We’ll be [Dm7/A]singing for - [G13]ev [G13(#5)]- [Gm7/C]er, a - [F]men[A7(#9)/E][Dm11]
+We’ll be [Dm7/A]singing for - [G13]ever and [G13(#5)]ever, [Gm7/C] a - [F]men
+
+{c:Interlude 1 (4x) *5}
+[Dm7][/][C/E][/][|][F][/][Bb2][/][|]
+
+{c:Bridge (3x) *6-7}
+[Dm7] If you've [C/E]ever wondered[F] What [Bb2]Heaven looks like
+[Dm7] [C/E] It’s looking like me [F]and you[Bb2]
+[Dm7] If you've [C/E]ever questioned[F] What [Bb2]Heaven sounds like
+[Dm7] [C2/E] Just let it fill [F]the room[Bb2]
+
+{c:Tag 1 (8x) *8}
+[Dm7] [C/E] Just let it fill [F]the room[Bb2]
+
+{c:Tag 2 (4x) *9}
+He's coming, He's [x]coming[x][x][x][|][x][x][x]
+
+{c:Tag 3 (8x) *10}
+He's coming, He's [Dm7]coming, [C/E] He's coming, He's [F]coming[Bb2]
+
+{c:Interlude 2 (2x) *11}
+[Bb2][/][/][/][|][/][/][/][/][|][/][/][/][/][|][/][/][/][/][|]
+
+{c:Tag 4 *12}
+[C/Bb]All [F/Bb]hail King [Bbma7]Jesus
+[C/Bb]All [F/Bb]hail King [Bbma7]Jesus
+
+{c:Tag 5 (2x) *13}
+[C/E]All [F]hail King [Bbma7]Jesus
+[C/E]All [F]hail King [Bbma7]Jesus
+
+{c:Tag 6 *14}
+[C/E]All [F]hail King [x]Je - [F13]sus[Ebma7]
+[Bbma9] All [A7(#5)]hail King [x]Je - [F13]sus[Ebma7]
+[Bbma9] All [A7(#5)]hail King [x]Je - [F13]sus[Ebma7]
+[Bbma9] All [A7(#5)]hail King [x]Je - [F13]sus[Ebma7]
+[Bbma9][|][/][/][/][|][/][/][/][/][|][/][/][/][/][|][/][/][/][|]
+
+{c:Interlude 3 *15}
+[Dm7][/][C/E][/][|][Eb/F][F9/A][Bbma9][A7(#9#5)][|]
+[Dm9][/][C/E][/D][-][/Db][|][Cm7][F13][Bbma9][/][A7(#9#5)][|]
+
+{c:Tag 7 (4x) *16-17}
+[Dm7]If you wanna know [C/E]what Heaven looks [Eb/F]like
+        F9/A    Bbma9    A7(#9#5)
+Lookin' like me and   you
+[Dm7]If you wanna know [C/E]what Heav - [Dm7]en [Eb/Db]sounds [Cm7]like
+         F13      Bbma9     A7(#9#5)
+Just let it  fill the   room
+
+{c:Interlude 4 *18}
+[Dm9][/][C/E][/][|][F9/A][Bbma9][/][A7(#9#5)][|]
+[Dm9][/][C/E][/D][-][/Db][-][/C][|][F13][Bbma9][/][A7(#9#5)][|]
+
+{c:Tag 8 (2x) *19}
+If you [Dm9]wanna know [C/E]what Heaven looks like
+        F9/A          Bbma9        A7(#9#5)
+Lookin' like, lookin' like  me and you
+If you [Dm9]wanna know [C/E]what Heav - [/D]en [/Db]looks [/C]like
+     F13          Bbma9             A7(#9#5)
+Just let it, just let   it fill the room
+
+{c:Tag 9 *20a}
+[Dm9]If you wanna know [C/E]what Heaven looks [Ebma7/F]like
+        F9/A          Bbma9    A7(#9#5)
+Lookin' like, lookin' like  me and      you
+[Dm9]If you wanna know [C7/E]what Heav - [/D]en [/Db]sounds [Cm7]like
+     F13          Bbma9             A7(#9#5)
+Just let it, just let   it fill the room
+
+{c:Tag 10 *20b}
+[Dm9]If you wanna know [C/E]what Heaven looks [Ebma7/F]like
+        F9/A          Bbma9    A7(#9#5)
+Lookin' like, lookin' like  me and      you
+[Dm9]If you wanna know [C7/E]what Heav - [/D]en [/Db]sounds [Cm7]like
+Just [F13]let it, just [Bbma9]let it fill the room
 `.substring(1);
 
 // Initialize CodeMirror instance
@@ -25,6 +129,8 @@ const editor = CodeMirror(document.getElementById('editor'), {
   lineNumbers: true,
   value: chordpro
 });
+
+editor.setSize("100%", "90%");
 
 // Function to render PDF in an <iframe> or <object>
 const renderPDFInBrowser = async (pdfBlob) => {
