1f881ae556505bd8c8fb50a5ec1835ae3ef69046
Martijn Versluis <martijnversluis@users.noreply.github.com>
2025-04-24 21:54:24 +0200

    Expose package version (#1659)
    ```js
    import { version } from 'chordsheetjs';
    console.log(version);
    
    // or:
    
    import ChordSheetJS from 'chordsheetjs';
    console.log(ChordSheetJS.version);
    ```

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ import Tag from './chord_sheet/tag';
 import Ternary from './chord_sheet/chord_pro/ternary';
 import TextFormatter from './formatter/text_formatter';
 import UltimateGuitarParser from './parser/ultimate_guitar_parser';
+import version from './version';
 
 import {
   each,
@@ -78,6 +79,7 @@ export { default as Ternary } from './chord_sheet/chord_pro/ternary';
 export { default as TextFormatter } from './formatter/text_formatter';
 export { default as UltimateGuitarParser } from './parser/ultimate_guitar_parser';
 export { default as templateHelpers } from './template_helpers';
+export { default as version } from './version';
 
 export {
   ABC,
@@ -125,6 +127,7 @@ export default {
   TextFormatter,
   UltimateGuitarParser,
   VERSE,
+  version,
   templateHelpers: {
     isEvaluatable,
     isChordLyricsPair,
